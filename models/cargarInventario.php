36a53e4e3af66d085f4cfd9abc35f5c38d7ef77d
José Pazmiño <pepe.paz0399@gmail.com>
2021-09-08 15:09:57 -0500

    terminar reportes

--- models/cargarInventario.php
+++ models/cargarInventario.php
@@ -1,6 +1,6 @@
 <?php
 include("../controllers/conexion.php");
-$sqlSelect="SELECT P.ID_PRO, P.NOM_PRO, P.MAR_PRO,P.EST_PRO,D.CAN_PRO_BOD, B.NOM_BOD FROM PRODUCTOS P, DETALLE_BODEGAS D, BODEGAS B WHERE P.ID_PRO = D.ID_PRO_BOD && D.ID_BOD_PER = B.ID_BOD";
+$sqlSelect="SELECT P.ID_PRO, P.NOM_PRO, P.MAR_PRO,P.EST_PRO,D.CAN_PRO_BOD, B.NOM_BOD FROM PRODUCTOS P, DETALLE_BODEGAS D, BODEGAS B WHERE P.ID_PRO = D.ID_PRO_BOD AND D.ID_BOD_PER = B.ID_BOD";
 $respuesta=$conexion->query($sqlSelect);
 $result=array();
 if($respuesta->num_rows>0){
